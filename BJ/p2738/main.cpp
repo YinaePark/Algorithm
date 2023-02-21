2c1153f458d951fe3a0111ff65bc48f042d5a104
Yinae Park <dlsoso1@unist.ac.kr>
2023-02-21 15:07:25 +0900

    feat: p2738

---
+++ BJ/p2738/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+// memset 쓰고 싶은데 include path 에러 생김
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+// n*m array allocation
+    int** sumArr = new int*[n];
+    for(int i=0; i<n; i++){
+        sumArr[i] = new int[m];
+    }
+// n*m array initialization
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            sumArr[i][j] = 0;
+        }
+    }
+
+    int elem;
+
+// input A, B
+    for(int k=0; k<2; k++){    
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                cin >> elem;
+                sumArr[i][j] += elem;
+            }
+        }
+    }
+
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << sumArr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+// n*m array deallocation
+    for(int i=0; i<n; i++){
+        delete[] sumArr[i];
+    }
+    delete[] sumArr;
+}
