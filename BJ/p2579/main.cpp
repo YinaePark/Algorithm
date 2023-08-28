ddb65265fbc9b6ee2a3f277d12ecff6e0f4624fb
YinaePark <dlsoso1@unist.ac.kr>
2023-08-28 16:15:05 +0900

    feat: p2579

--- BJ/p2579/main.cpp
+++ BJ/p2579/main.cpp
@@ -5,33 +5,39 @@ using namespace std;
 
 
 // 0 : 1개 1: 2개
-int dp[301][2];
+int dp[301];
 int stair[301];
+int continueNum = 1;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    int continueCnt = 0;
-    int maxSum = 0;
+    int n; 
     cin >> n;
 
     for(int i=1; i<=n; i++){
         cin >> stair[i];
     }
 
-    dp[1][0] = dp[1][1] = stair[1];
-    if(n == 1){
-        cout << dp[1][0] << "\n";
+    dp[1] = stair[1];
+    dp[2] = stair[2] + stair[1];
+    dp[3] = max(stair[1], stair[2]) + stair[3];
+
+    if(n < 4){
+        cout << dp[n] << "\n";
         return 0;
     }
-    int curr=2;
+
+
+    int curr=3;
     while(curr <= n){
-        dp[curr-1]
-        if(n==2) break;
+        dp[curr] = max(dp[curr-2]+stair[curr], stair[curr-1]+stair[curr]+dp[curr-3]);
+        curr++;
     }
+    cout << dp[n] << endl;
+
     
  
 }
